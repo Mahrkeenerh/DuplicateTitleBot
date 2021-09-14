86d41e33d7b3efd0eb5881af7b76e4c3950ca36b
Mahrkeenerh <samuelbuban@gmail.com>
2021-09-14 16:38:40 +0200

    Change remove function

--- bscbot.py
+++ bscbot.py
@@ -165,7 +165,7 @@ while True:
                 duplicate, link = is_duplicate(submission, submission_time)
                 if duplicate:
                     submission.reply("Your post has been removed, because it has been marked as a duplicate of: https://www.reddit.com%s\n\nTo prevent spam, you can only post once per day.\n\nThis action was performed automatically." % link)
-                    submission.delete()
+                    submission.mod.remove()
                 
                 else:
                     data_dict[submission.title] = {"time": submission_time.strftime('%y.%m.%d %H:%M:%S'), "link": submission.permalink}
